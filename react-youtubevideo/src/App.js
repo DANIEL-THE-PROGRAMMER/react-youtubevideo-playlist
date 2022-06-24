231ba6a00be53deed8c24a61df77206b2ae12d3d
Daniel-TheProgrammer <njid18753@gmail.com>
2022-06-24 04:31:06 +0100

    getting started with react

---
+++ react-youtubevideo/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <div>
+        <h2>Getting Started with React</h2>
+      </div>
+      <div>Test</div>
+      <div>Test 2</div> <div>Test 3</div>
+    </div>
+  );
+}
+
+export default App;
